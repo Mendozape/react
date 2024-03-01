95955a0277be7fbc92697446b4856b01a9f21dbc
Mendozape <erasto.mendoza.perez@gmail.com>
2024-03-01 13:48:04 -0600

    React Installed

---
+++ resources/js/app.js
@@ -0,0 +1,2 @@
+import './bootstrap';
+import './Codea.jsx'
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+@section('title', 'Dashboard')
+@section('content_header')
+	<div id="header"></div>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel 10 - REACT 18</title>
+@stop
+@section('content')
+	<div id="codeareact"></div>
+@stop
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+@section('js')
+	@viteReactRefresh
+	@vite('resources/js/app.js')
+@stop
+
