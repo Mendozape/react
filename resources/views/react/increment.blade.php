3815c8ce93818f261f9e43b2201b3a2b9d3361ce
Mendozape <erasto.mendoza.perez@gmail.com>
2024-03-08 14:58:52 -0600

    Component

--- resources/views/react/increment.blade.php
+++ resources/views/react/increment.blade.php
@@ -3,18 +3,16 @@
 @section('content_header')
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-
+    
 	<title>Laravel 10 - REACT 18 INCREMENT</title>
 @stop
 @section('content_top_nav_right')
 	<div id="TopNavDiv"></div>
 @stop
 @section('content')
-	<div id="increment"></div>
+	<div className="container" id="increment"></div>
 @stop
 @section('css')
-    @vite('resources/sass/app.scss')
 @stop
 @section('js')
 	@viteReactRefresh
